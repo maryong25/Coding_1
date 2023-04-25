8207dc47d1000a2f1d755b0fb1d3710d2bfb5d37
Mary Yong <maryong2525@gmail.com>
2023-04-25 20:44:14 +0200

    Christian Homework

---
+++ Excersice 12- Coding Practice/noscript.js
@@ -0,0 +1,60 @@
+//<!-- This was the first my first try. Then I had to aks chat gpt to help me get the progress bar in
+// as I didnt know how to get the checked checkboxes count -->
+
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+    var ul = document.getElementById('ul'); //ul
+    var li = document.createElement('li');//li
+    var text = document.getElementById('texto');
+    let count = 0;
+    
+
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    checkbox.id = "id";
+    //checkbox.onclick = myFunction();
+    //checkbox.checked= false;
+
+    
+
+    if (text.value === '') {
+            alert("You must write something!");
+        } else {
+            li.appendChild(checkbox);
+            li.appendChild(document.createTextNode(text.value));
+            ul.appendChild(li); 
+
+            count = +1;
+            console.log (count);
+
+        }
+
+}
+
+
+
+
+
+
+
+var button = document.getElementById('btn');
+    button.onclick = newElement 
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
---
+++ Excersice 12- Coding Practice/script.js
@@ -0,0 +1,64 @@
+// Get references to the input field, button, and list
+const newTaskInput = document.getElementById("new-task");
+const addTaskButton = document.getElementById("add-task");
+const taskList = document.getElementById("task-list");
+const progress = document.querySelector("progress");
+
+// Keep track of the number of completed tasks
+let completedTasks = 0;
+
+
+// Define the click event handler for the button
+
+function addTask() {
+  // Get the value of the input field
+  const newTaskText = newTaskInput.value;
+
+  // Create a new list item element with the new task text
+  const newTaskListItem = document.createElement("li");
+
+
+
+  // Create a new checkbox element and add it to the list item
+  const newTaskCheckbox = document.createElement("input");
+  newTaskCheckbox.type = "checkbox";
+  newTaskListItem.appendChild(newTaskCheckbox);
+
+
+
+  // Add the new task text to the list item
+  const newTaskTextSpan = document.createElement("span");
+  newTaskTextSpan.textContent = newTaskText;
+  newTaskListItem.appendChild(newTaskTextSpan);
+
+   // If the input is empty, show an alert message and do nothing
+   if (newTaskText.trim() === "") {
+    alert("Please enter a task");
+
+    return;
+  }
+
+  // Add event listener for when the checkbox is clicked
+  newTaskCheckbox.onclick = function() {
+
+    if (newTaskCheckbox.checked) {
+      // Increment the number of completed tasks and update the progress bar
+      completedTasks++;
+      progress.value = completedTasks;
+    } else {
+      // Decrement the number of completed tasks and update the progress bar
+      completedTasks--;
+      progress.value = completedTasks;
+    }
+
+  };
+
+  // Append the new task item to the list
+  taskList.appendChild(newTaskListItem);
+
+  // Update the max value of the progress bar
+  progress.max = taskList.children.length;
+
+  // Clear the input field
+  newTaskInput.value = "";
+}
